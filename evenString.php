26dc1fa1d02e2edb0c6fa9049a6c4c064f169c8c
Sanvi Rahman <SanviRahman>
2025-05-06 14:35:33 +0600

    last

--- evenString.php
+++ evenString.php
@@ -4,16 +4,18 @@
     $string=trim(fgets(STDIN));
 
     $length=strlen($string);
+    
     $evenNumber="";
-
     for($i=0; $i<$length;$i++){
-        $digit=intval($string[$i]);
-        if($digit%2==0){
-            $evenNumber .= $digit ;
+        $char=($string[$i]);
+        if($char==='0' || $char==='2' || $char==='4'
+            || $char==='6' || $char==='8')
+        {
+            $evenNumber .= $char ;
         }
     }
-    echo ("The even Number is: ". $evenNumber ."\n");
-    
+    echo ("The even number is: ". $evenNumber ."\n");
+
     echo ("Even number divisor are: ");
     for($i=1; $i<$evenNumber; $i++){
         if($evenNumber%$i==0){

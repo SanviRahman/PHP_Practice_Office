abc5632c4da3e938c2c206a52b3f9caef66d5f0f
Sanvi Rahman <SanviRahman>
2025-05-06 19:16:36 +0600

    chuti

---
+++ object.php
@@ -0,0 +1,16 @@
+<?php
+    class Car{
+        public $color;
+        public $model;
+        public function __construct($color,$model)
+        {
+            $this->color=$color;
+            $this->model=$model;
+        }
+    }
+    $myCar= new Car("Navy Blue","Tesla");
+
+    foreach($myCar as $x=>$y){
+       echo "$x: $y". "\n";
+    }
+?>

27de516ae541473f6ae7bf8f082bcec7c5aa654c
Sanvi Rahman <SanviRahman>
2025-05-15 15:13:13 +0600

    Sanvi

--- depenInject.php
+++ depenInject.php
@@ -7,22 +7,56 @@ class Engine
         echo "Engine Started!\n";
     }
 }
+class BreakDown
+{
+    public function end()
+    {
+        echo "Engine Stoped\n";
+    }
+}
+class Music
+{
+    public function music()
+    {
+        echo "Play the Music".PHP_EOL;
+    }
+}
 class Car
 {
     private $engine;
+    private $break;
+    private $music;
 
-    public function __construct(Engine $engine)
+    public function __construct(Engine $engine, BreakDown $break,Music $music)///enject kora holo
     {
         $this->engine= $engine;
+        $this->break= $break;
+        $this->music=$music;
     }
 
     public function startCar()
     {
         $this->engine->start();
-        echo "Car is running!";
+        echo "Car is running!".PHP_EOL;
+    }
+    public function endCar()
+    {
+        $this->break->end();
+        echo "Car is stopping!".PHP_EOL;
+    }
+
+    public function musicCar()
+    {
+        $this->music->music();
+        echo "Music is playing" .PHP_EOL;
     }
 }
 
 $engine=new Engine();
-$car= new Car($engine);
+$break=new BreakDown();
+$music=new Music();
+
+$car= new Car($engine,$break,$music);
 $car->startCar();
+$car->endCar();
+$car-> musicCar();
